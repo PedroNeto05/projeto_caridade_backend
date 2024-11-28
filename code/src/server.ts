dfcbd4ec6c714ca8e829dc3751ecf1d4a6b8e2f9
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-11-28 09:44:49 -0300

    change  project struct

--- code/src/server.ts
+++ code/src/server.ts
@@ -1,5 +1,5 @@
-import { env } from '@/env';
-import { app } from './app';
+import { app } from '@/app/app';
+import { env } from '@/app/lib/env';
 
 app.listen(env.PORT, () => {
   console.log(`Server running on port ${env.PORT}`);
