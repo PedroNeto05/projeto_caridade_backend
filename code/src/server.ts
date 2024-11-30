fcb749e9554ff896ce17257436ef059a73d87c70
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-11-30 00:05:08 -0300

    chore: change import path

--- code/src/server.ts
+++ code/src/server.ts
@@ -1,5 +1,5 @@
-import { app } from '@/app/app';
-import { env } from '@/app/lib/env';
+import { app } from './app/app';
+import { env } from './app/config/env';
 
 app.listen(env.PORT, () => {
   console.log(`Server running on port ${env.PORT}`);
