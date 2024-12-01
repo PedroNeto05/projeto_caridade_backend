2d1d041d73b416bea579e1fd82f7c4a55f3e69a4
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-12-01 17:16:17 -0300

    feat: update update event schema

--- code/src/app/schemas/event.schema.ts
+++ code/src/app/schemas/event.schema.ts
@@ -2,8 +2,8 @@ import { z } from 'zod';
 
 export const createEventSchema = z.object({
   userId: z.number(),
-  name: z.string(),
-  location: z.string(),
+  name: z.string().min(5),
+  location: z.string().min(10),
   date: z.string().transform((val) => new Date(val)),
   description: z.string().optional(),
 });
@@ -13,9 +13,10 @@ export const updateEventSchema = z.object({
   eventId: z.number(),
   newDate: z
     .string()
+    .date()
     .transform((val) => new Date(val))
     .optional(),
-  newName: z.string().optional(),
+  newName: z.string().min(5).optional(),
   newDescription: z.string().optional(),
-  newLocation: z.string().optional(),
+  newLocation: z.string().min(10).optional(),
 });
