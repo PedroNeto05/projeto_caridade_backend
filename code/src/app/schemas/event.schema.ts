4108ad4471c509efe8f5a3c66ed8d2faf6005252
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-12-01 21:02:00 -0300

    fix: update update event schema to use datetime

--- code/src/app/schemas/event.schema.ts
+++ code/src/app/schemas/event.schema.ts
@@ -4,19 +4,14 @@ export const createEventSchema = z.object({
   userId: z.number(),
   name: z.string().min(5),
   location: z.string().min(10),
-  date: z.string().transform((val) => new Date(val)),
+  date: z.string().datetime(),
   description: z.string().optional(),
 });
 
-// TODO ajeitar o date desse schema, pois esta validando errado
 export const updateEventSchema = z.object({
   userId: z.number(),
   eventId: z.number(),
-  newDate: z
-    .string()
-    .date()
-    .transform((val) => new Date(val))
-    .optional(),
+  newDate: z.string().datetime(),
   newName: z.string().min(5).optional(),
   newDescription: z.string().optional(),
   newLocation: z.string().min(10).optional(),
